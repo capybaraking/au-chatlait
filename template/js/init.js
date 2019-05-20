ee85d97ea22434ecd987e4e28f93d08178633cf4
capybaraking <julie.simonin9@etu.univ-lorraine.fr>
2019-05-20 15:52:39 +0200

    map edit

--- template/js/init.js
+++ template/js/init.js
@@ -12,8 +12,26 @@
     accessToken: 'pk.eyJ1IjoicmFhbGtlbnpvIiwiYSI6ImNqdndkOHl1dTN5Z3A0Ym1neWNmbTNweWsifQ.d2tQXU8qwaNFzokYLyAZGA'
     }).addTo(mymap);
 
-    var marker = L.marker([48.688, 6.187]).addTo(mymap);
+    var redIcon = new L.Icon({
+      iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+
+    var marker = L.marker([48.688, 6.187],{icon: redIcon}).addTo(mymap);
+    
+    var markerP1 = L.marker([48.6898, 6.182]).addTo(mymap);
+    
+    var markerP2 = L.marker([48.6897, 6.189]).addTo(mymap);
+
+
     marker.bindPopup("<b>Au chat-lait</b><br>XX Rue Saint Nicolas NANCY").openPopup();
+    markerP2.bindPopup("<b>Parking 2</b><br>Ecole XXX").openPopup();
+
+    markerP1.bindPopup("<b>Parking 1</b><br>Charles III").openPopup();
 
 
   }); // end of document ready
